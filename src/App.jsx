cac9967c61cb550681ba83464615a00d2f9e6595
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-10-17 16:51:29 +0530

    import QueryClientProvider & QueryClient

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import Gallery from "./components/Gallery";
 import SearchForm from "./components/SearchForm";
 import ThemeToggle from "./components/ThemeToggle";
+import {QueryClientProvider, QueryClient} from '@tanstack/react-query'
 
 const App = () => {
+
+  const queryClient = new QueryClient()
+
   return <main>
-    <ThemeToggle/>
-    <SearchForm/>
-    <Gallery/>
+    <QueryClientProvider client={queryClient}>
+      <ThemeToggle/>
+      <SearchForm/>
+      <Gallery/>
+    </QueryClientProvider>
   </main>;
 };
 export default App;
