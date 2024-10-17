c6bffea89c16dfd26217e0d7100fa4e6aaf32777
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-10-17 11:09:56 +0530

    import icon and set it to button

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,6 +1,14 @@
+import {BsFillSunFill, BsFillMoonFill} from 'react-icons/bs'
+import { useGlobalContext } from '../context'
+
 const ThemeToggle = () => {
+    const {isDarkTheme, toggleDarkTheme} = useGlobalContext()
   return (
-    <div>ThemeToggle</div>
+    <section className="toggle-container">
+        <button className="dark-toggle" onClick={toggleDarkTheme}>
+            {isDarkTheme? <BsFillSunFill className='toggle-icon'/> : <BsFillMoonFill className='toggle-icon'/> }            
+        </button>
+    </section>
   )
 }
 
