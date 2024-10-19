c8f42e430a77852e18557817de6645d578b7ff96
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-10-19 14:00:01 +0530

    set useEffect funtionality to detect intial prefer theme

--- src/context.jsx
+++ src/context.jsx
@@ -2,8 +2,14 @@ import { createContext, useContext, useState, useEffect } from "react";
 
 const AppContext = createContext()
 
+const getInitialDarkMode = () => {
+    const prefersDarkMode = window.matchMedia('(prefers-color-scheme:dark)').matches
+    console.log(prefersDarkMode);
+    return prefersDarkMode;
+}
+
 export const AppProvider = ({children}) => {
-    const [isDarkTheme, setIsDarkTheme] = useState(false)
+    const [isDarkTheme, setIsDarkTheme] = useState(getInitialDarkMode())
     const [searchTerm, setSearchTerm] = useState("panda")
 
     const toggleDarkTheme = () => {
@@ -19,6 +25,10 @@ export const AppProvider = ({children}) => {
         */
     }
 
+    useEffect(() => {
+        document.body.classList.toggle('dark-theme', isDarkTheme)
+    }, [])
+    
     return <AppContext.Provider value={{isDarkTheme, toggleDarkTheme, searchTerm, setSearchTerm}}>
         {children}
     </AppContext.Provider>
