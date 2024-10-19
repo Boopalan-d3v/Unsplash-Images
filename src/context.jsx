4950439878555340ca8f30114f5509f8a13e3607
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-10-19 16:41:16 +0530

    store DarkMode state on localStorage

--- src/context.jsx
+++ src/context.jsx
@@ -3,9 +3,18 @@ import { createContext, useContext, useState, useEffect } from "react";
 const AppContext = createContext()
 
 const getInitialDarkMode = () => {
-    const prefersDarkMode = window.matchMedia('(prefers-color-scheme:dark)').matches
+    /* const prefersDarkMode = window.matchMedia('(prefers-color-scheme:dark)').matches
+    const storedDarkMode = localStorage.getItem('darkTheme') === 'true';
+
     console.log(prefersDarkMode);
-    return prefersDarkMode;
+    return storedDarkMode || prefersDarkMode; */
+
+    const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const storedDarkMode = localStorage.getItem('darkTheme');
+        if (storedDarkMode === null) {
+            return prefersDarkMode;
+        }
+    return storedDarkMode === 'true';
 }
 
 export const AppProvider = ({children}) => {
@@ -15,19 +24,20 @@ export const AppProvider = ({children}) => {
     const toggleDarkTheme = () => {
         const newDarkTheme = !isDarkTheme
         setIsDarkTheme(newDarkTheme);
-        const body = document.querySelector('body')
-        body.classList.toggle('dark-theme', newDarkTheme)
-        console.log(body)
+        localStorage.setItem('darkTheme', newDarkTheme)
+
+
         /* 
         ### Alternative for select body
 
         const body = document.classList.toggle('dark-theme', newDarkTheme)
+        console.log(body)
         */
     }
 
     useEffect(() => {
         document.body.classList.toggle('dark-theme', isDarkTheme)
-    }, [])
+    }, [isDarkTheme])
     
     return <AppContext.Provider value={{isDarkTheme, toggleDarkTheme, searchTerm, setSearchTerm}}>
         {children}
