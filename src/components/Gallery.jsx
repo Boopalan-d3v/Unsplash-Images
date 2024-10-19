abc326b6becf16e527a36f4fbeb3e96de1aec83e
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-10-19 13:37:55 +0530

    set searchTerm on queryKey array

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,14 +1,17 @@
 import { useQuery } from "@tanstack/react-query"
 import axios from 'axios'
+import { useGlobalContext } from "../context"
 
-const url = "https://api.unsplash.com/search/photos?client_id=psG9dR9gsB24d6KpoMJgyvTUtklgMkSDqmmGU72XI8I&query=panda"
+const url = "https://api.unsplash.com/search/photos?client_id=psG9dR9gsB24d6KpoMJgyvTUtklgMkSDqmmGU72XI8I"
 
 const Gallery = () => {
 
+  const { searchTerm } = useGlobalContext()
+
   const response = useQuery({
-    queryKey:['images'],
+    queryKey:['images', searchTerm],
     queryFn: async() => {
-      const result = await axios.get(url);
+      const result = await axios.get(`${url}&query=${searchTerm}`);
 
       return result.data;
     }
